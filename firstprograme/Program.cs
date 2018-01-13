c1a2f7daf2a6b472451aaae12fe1152c200f7769
taha <taha@WW>
2018-01-13 16:36:45 +0100

    deff \ float/double/decimale

--- firstprograme/Program.cs
+++ firstprograme/Program.cs
@@ -4,15 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace firstprograme
+namespace data_type.float_point_type
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //this is my first programe seccesfull
-            //1525634try
-            Console.WriteLine("hello world");
+            float flmin = float.MinValue;
+            float flmax = float.MaxValue;
+
+            Console.WriteLine("float min{0} float max{1}", flmin, flmax);
+
+
+            float flvar = 1f / 3;
+            double dbvar = 1d / 3;
+            decimal dlvar = 1m / 3;
+
+            Console.WriteLine("\nfloat var{0} \ndouble var {1} \ndecimale var {2}", flvar, dbvar, dlvar);
+
             Console.ReadKey();
         }
     }
